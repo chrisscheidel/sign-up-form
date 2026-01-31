b12ee1e62d0a41a2d8b87a3f491ab6c5a23954a8
chrisscheidel <146884440+chrisscheidel@users.noreply.github.com>
2026-01-31 13:57:59 -0500

    make positioning adapt to screen

--- positioning.js
+++ positioning.js
@@ -1,7 +1,13 @@
-const inputForm = document.querySelector(".input-form");
-const overlay = document.querySelector(".overlay");
+function alignOverlay() {
+    const inputForm = document.querySelector(".input-form");
+    const overlay = document.querySelector(".overlay");
 
-const rect = inputForm.getBoundingClientRect();
-const distanceFromBottom = window.innerHeight - rect.top;
+    const rect = inputForm.getBoundingClientRect();
+    const distanceFromBottom = window.innerHeight - rect.top;
 
-overlay.style.bottom = distanceFromBottom + "px"
+    overlay.style.bottom = distanceFromBottom + "px"
+};
+
+window.addEventListener("resize", alignOverlay);
+window.addEventListener("scroll", alignOverlay);
+alignOverlay();
